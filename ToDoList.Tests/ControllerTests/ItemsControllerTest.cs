7022d1e6ac6e77f809d4f6d7e12d9dd1bac5a67f
Adam Titus and James Osborn <adam+james@>
2018-01-09 14:38:18 -0800

    add Save, Edit, Remove to EFItemRepository

---
+++ ToDoList.Tests/ControllerTests/ItemsControllerTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ToDoList.Models;
+using ToDoList.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ToDoList.Tests
+{
+	[TestClass]
+	public class ItemControllerTests
+	{
+		[TestMethod]
+		public void ItemsController_AddsItemToIndexModelData_Collection()
+		{
+			// Arrange
+			ItemsController controller = new ItemsController();
+			Item testItem = new Item();
+			testItem.Description = "test item";
+            testItem.CategoryId = 1;
+
+			// Act
+			controller.Create(testItem);
+			ViewResult indexView = new ItemsController().Index() as ViewResult;
+			var collection = indexView.ViewData.Model as List<Item>;
+
+            foreach(var thing in collection)
+            {
+                Console.WriteLine("############" + thing.ItemId + thing.Description);
+            }
+
+			// Assert
+			CollectionAssert.Contains(collection, testItem);
+		}
+	}
+}
---
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -0,0 +1,25 @@
+using System;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ToDoList.Models;
+
+namespace ToDoList.Tests
+{
+    [TestClass]
+    public class ItemTests
+    {
+        [TestMethod]
+        public void GetDescription_ReturnsItemDescription_String()
+        {
+            //Arrange
+            var item = new Item();
+            item.Description = "Wash the dog";
+
+            //Act
+            var result = item.Description;
+
+            //Assert
+            Assert.AreEqual("Wash the dog", result);
+        }
+    }
+}
